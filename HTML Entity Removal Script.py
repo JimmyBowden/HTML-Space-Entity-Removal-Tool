454e4158da5ab07eb9fd9cbc1162e39bf14c2b6c
JimmyBowden <32491606+JimmyBowden@users.noreply.github.com>
2018-01-08 14:51:29 -0500

    Add files via upload

---
+++ HTML Entity Removal Script.py
@@ -0,0 +1,17 @@
+#HTML Space Removal Script
+#Author Jimmy M Bowden Jr.
+#Purpose of this scrip is to remove HTML space entities like "&nbsp;" and any "double" spaces
+#This is being created to remove the need to use notepad++ to remove spaces and streamline the process
+#C:\Users\James Bowden\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.6\python.exe
+#C:\Users\James Bowden\Documents\HTML Removal Script.py
+#---------------------Variables------------------------
+userInputPromptString = "Hello User, please enter the text that you wish to have processed and hit Enter to continue."
+
+#---------------------Methods--------------------------
+
+
+#---------------------Main Process---------------------
+inputString = raw_input(userInputPromptString)
+print(inputString)
+
+
