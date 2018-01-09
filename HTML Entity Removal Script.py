889034134dc4285e0394e5dcc567368ec67cbb6a
JimmyBowden <32491606+JimmyBowden@users.noreply.github.com>
2018-01-08 22:28:31 -0500

    Update HTML Entity Removal Script.py

--- HTML Entity Removal Script.py
+++ HTML Entity Removal Script.py
@@ -2,16 +2,87 @@
 #Author Jimmy M Bowden Jr.
 #Purpose of this scrip is to remove HTML space entities like "&nbsp;" and any "double" spaces
 #This is being created to remove the need to use notepad++ to remove spaces and streamline the process
-#C:\Users\James Bowden\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.6\python.exe
-#C:\Users\James Bowden\Documents\HTML Removal Script.py
+
+from tkinter import Tk
+
 #---------------------Variables------------------------
-userInputPromptString = "Hello User, please enter the text that you wish to have processed and hit Enter to continue."
+#strings
+userInputPromptString = "Hello User, please enter the text that you wish to have processed and hit Enter to continue. "
+altString1 = ""
+altString2 = ""
 
-#---------------------Methods--------------------------
+#ints
+HTMLNBSpaceCount = 0
+doubleSpaceCount = 0
 
+#testing Vars
+testString1 = "Test  test  test &nbsp;  test test"
+testString2 = "test&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;test"
+
+#---------------------Function--------------------------
+
+#HTMLNBSpaceReplace(inputString)
+#This is used to parse a string for any &nbsp; and replace it with a space
+#Returns a tuple with a string and count
+def HTMLNBSpaceReplace(inputString):
+    print(inputString)
+    HTMLNBSpaceCount = inputString.count("&nbsp;")
+    altString1 = inputString.replace("&nbsp;", " ")
+    HTMLNBSpaceTuple = (HTMLNBSpaceCount, altString1)
+    return HTMLNBSpaceTuple
+
+#doubleSpaceReplace(inputString)
+#This is used to parse a string for double spaces and convert them into single spaces
+#Recurses until all strings are single strings
+def doubleSpaceReplace(inputString):
+    doubleSpaceCount = inputString.count("  ")
+    if doubleSpaceCount == 0:
+        altString2 = inputString
+        doubleSpaceTuple = (doubleSpaceCount, altString2)
+        return doubleSpaceTuple
+
+    else:
+        altString = inputString.replace("  ", " ")
+        altString2 = altString.replace("  ", " ")
+        return doubleSpaceReplace(altString2)
+
+
+#copyToClipboard(inputString)
+#This is used to copy the final string into the clipboard
+def copyToClipboard(inputString):
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(inputString)
+    r.update() # now it stays on the clipboard after the window is closed
+    r.destroy()
+    return
+
+    
 
 #---------------------Main Process---------------------
-inputString = raw_input(userInputPromptString)
-print(inputString)
+
+#Intro to user and ask for string
+#ask for string
+print(userInputPromptString)
+inputString = input()
+
+#parse string for &nbsp; and replace with a space
+HTMLNBSpaceTuple = HTMLNBSpaceReplace(inputString)
+altString1 = HTMLNBSpaceTuple[1]
+HTMLNBSpaceCount = HTMLNBSpaceTuple[0]
+
+
+#parse new version of string for double space
+doubleSpaceTuple = doubleSpaceReplace(altString1)
+altString2 = doubleSpaceTuple[1]
+doubleSpaceCount = doubleSpaceTuple[0]
+
+
+#copy final string into clipboard
+copyToClipboard(altString2)
+print("There were " + str(HTMLNBSpaceCount) + " &nbsp; in the original string.")
+print("There were " + str(doubleSpaceCount) + " double spaces in the original string.")
+print("The result is now in your clipboard. You can now paste it.")
 
 
